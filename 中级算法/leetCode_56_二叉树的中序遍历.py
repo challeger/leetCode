e755434ff456344f4ab48ef3894c95d335e0a4f0
ylnx <799613500@qq.com>
2020-08-17 22:34:12 +0800

    二叉树的中序遍历-新增标记写法

--- 中级算法/leetCode_56_二叉树的中序遍历.py
+++ 中级算法/leetCode_56_二叉树的中序遍历.py
@@ -11,8 +11,15 @@ url: https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnldjj/
  1  3 15  7
    输出: [1, 9, 3, 2, 15, 10, 7]
 思路:
+1. 栈
 利用栈后进先出,将节点一直往左孩子移,直到为空,然后弹出栈,将值添加到结果中
 然后再指向节点的右孩子,若右孩子有左孩子,继续移动直到空,以此循环.
+2. 标记法
+若栈中存放的是一个节点将[它的右孩子,它的值,它的左孩子]推入栈中
+若栈中存放的是一个变量,将这个变量添加到res栈中
+
+若是前序遍历,则顺序为[左孩子,值,右孩子]
+后序遍历,顺序为[值,右孩子,左孩子]
 """
 
 
@@ -39,14 +46,23 @@ class Solution:
         # return res
 
         # 递归写法
-        res = []
+        # res = []
 
-        def helper(node):
-            if node:
-                if node.left:
-                    helper(node.left)
-                res.append(node.val)
-                if node.right:
-                    helper(node.right)
-        helper(root)
-        return res
+        # def helper(node):
+        #     if node:
+        #         if node.left:
+        #             helper(node.left)
+        #         res.append(node.val)
+        #         if node.right:
+        #             helper(node.right)
+        # helper(root)
+        # return res
+        # 标记法
+        stack, rst = [root], []
+        while stack:
+            node = stack.pop()
+            if isinstance(node, TreeNode):
+                stack.extend([node.right, node.val, node.left])
+            elif isinstance(node, int):
+                rst.append(node)
+        return rst
